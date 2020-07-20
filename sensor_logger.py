941279446b7813a402e8bb4abc1bb1c4107d84cd
siddacious <nospam187+github@gmail.com>
2020-07-20 14:41:32 -0700

    updates

--- sensor_logger.py
+++ sensor_logger.py
@@ -28,12 +28,15 @@ class SensorLogger:
         except ImportError:
             raise RuntimeError("Settings file settings.py missing")
         self._error_log_file = settings['error_log_filename']
+        self.io = None
         self._i2c = board.I2C()
         self._initialize_wifi()
         self._initialize_sensors()
         self._initialize_io()
         self.battery = analogio.AnalogIn(board.BATTERY)
-
+        self.temperature_feed = None
+        self.humidity_feed = None
+        self.batt_volts_feed = None
     def _log_exceptions(func):
         # pylint:disable=protected-access
         def _decorator(self, *args, **kwargs):
@@ -41,7 +44,7 @@ class SensorLogger:
             try:
                 retval = func(self, *args, **kwargs)
                 return retval
-            except RuntimeError as e:
+            except Exception as e:
                 err_str = "error in %s\n%s\n"%(func.__name__, e)
                 if self._settings['log_errors_to_file']:
                     import storage
@@ -88,24 +91,21 @@ class SensorLogger:
         self.io = IO_HTTP(aio_username, aio_key, self.wifi)
         self._initialize_feeds()
 
+    @_log_exceptions
     def _initialize_feeds(self):
         self.temperature_feed = self.io.get_feed("temperature")
         self.humidity_feed = self.io.get_feed("humidity")
-        self.batt_volts_feed = self.io.get_feed("batt_volts")
+        self.batt_volts_feed = self.io.get_feed("batt-volts")
 
 
 
     @_log_exceptions
     def _initialize_sensors(self):
+        if not self.io:
+            self._initialize_io
         # Create library object using our Bus I2C port
         self.bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(self._i2c)
-
-        # change this to match the location's pressure (hPa) at sea level
-        self.bmp280.sea_level_pressure = 1013.25
         self.bmp280.mode = adafruit_bmp280.MODE_NORMAL
-        self.bmp280.standby_period = adafruit_bmp280.STANDBY_TC_500
-        self.bmp280.iir_filter = adafruit_bmp280.IIR_FILTER_X16
-        self.bmp280.overscan_pressure = adafruit_bmp280.OVERSCAN_X16
         self.bmp280.overscan_temperature = adafruit_bmp280.OVERSCAN_X2
 
         time.sleep(1)
@@ -113,12 +113,15 @@ class SensorLogger:
 
     @_log_exceptions
     def log_sensors(self):
+        print("verifying feeds")
+        if not (self.humidity_feed and self.temperature_feed and self.batt_volts_feed):
+            self._initialize_feeds()
         temp = self.bmp280.temperature
         hum = self.sht.relative_humidity
         bv = volts(self.battery)
-        print("\033[1mBMP Tmemperature:\033[0m %0.3f ºC" % self.sht.temperature)
+        print("\033[1mBMP Tmemperature:\033[0m %0.3f k ºC" % self.sht.temperature)
         print("\033[1mSHT Humidity:\033[0m %0.2f %%" % hum)
-        print("\033[1mBattery Voltage:\033[0m %0.2f %%" % bv)
+        print("\033[1mBattery Voltage:\033[0m %0.2f V" % bv)
         # Retrieve data value from the feed
         self.io.send_data(self.temperature_feed["key"], temp)
         self.io.send_data(self.humidity_feed["key"], hum)
@@ -130,10 +133,11 @@ class SensorLogger:
 last_write = time.monotonic()
 logger = SensorLogger()
 while True:
-    if not (self.humidity_feed and self.temperature_feed and self.batt_volts_feed):
-        self._initialize_feeds()
-        continue
+
     if abs(time.monotonic()-last_write >15):
         last_write = time.monotonic()
         logger.log_sensors()
-        print("took %d ns to send sensor data"%(time.monotonic()-write_start))
+        print("took %d seconds to send sensor data"%(time.monotonic()-last_write))
+
+# https://io.adafruit.com/api/v2/siddacious/feeds/batt-volts
+# https://io.adafruit.com/api/v2/siddacious/feeds/batt_voltsk
