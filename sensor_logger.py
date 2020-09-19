2c98a40a861e4a31ff2dd6b154088442a4e455aa
siddacious <nospam187+github@gmail.com>
2020-09-19 14:05:42 -0700

    mods to decouple sensor

--- sensor_logger.py
+++ sensor_logger.py
@@ -8,14 +8,32 @@ from digitalio import DigitalInOut
 from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
 import neopixel
 from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
-import adafruit_bmp280
-import adafruit_sht31d
+from adafruit_motorkit import MotorKit
+# import adafruit_bmp280
+# import adafruit_sht31d
+from adafruit_bme680 import Adafruit_BME680_I2C as PHT_SENSOR
 import time
 import analogio
+
+"""Simple test for using adafruit_motorkit with a DC motor"""
+import time
+
+
+
+i2c = board.I2C()
+# kit = MotorKit(i2c=i2c)
+
+# kit.motor1.throttle = 1.0
+# time.sleep(2)
+# kit.motor1.throttle = 0
+
+# time.sleep(1)
+
+
 def volts(analog_in):
     return 2*3.3*(analog_in.value/65535)
 class SensorLogger:
-    def __init__(self):
+    def __init__(self, i2c_bus=None):
                 # Get wifi details and more from a secrets.py file
         try:
             from secrets import secrets
@@ -27,13 +45,16 @@ class SensorLogger:
             self._settings = settings
         except ImportError:
             raise RuntimeError("Settings file settings.py missing")
+        if i2c_bus:
+            self._i2c = i2c_bus
+        else:
+            self._i2c = board.I2C()
         self._error_log_file = settings['error_log_filename']
         self.io = None
-        self._i2c = board.I2C()
         self._initialize_wifi()
         self._initialize_sensors()
         self._initialize_io()
-        self.battery = analogio.AnalogIn(board.BATTERY)
+        # self.battery = analogio.AnalogIn(board.BATTERY)
         self.temperature_feed = None
         self.humidity_feed = None
         self.batt_volts_feed = None
@@ -57,6 +78,7 @@ class SensorLogger:
 
             return retval
         return _decorator
+
     def _initialize_wifi(self):
 
         # Get wifi details and more from a secrets.py file
@@ -72,6 +94,9 @@ class SensorLogger:
             esp32_cs = DigitalInOut(board.D13)
             esp32_ready = DigitalInOut(board.D11)
             esp32_reset = DigitalInOut(board.D12)
+            # esp32_cs = DigitalInOut(board.ESP_CS)
+            # esp32_ready = DigitalInOut(board.ESP_BUSY)
+            # esp32_reset = DigitalInOut(board.ESP_RESET)
         except AttributeError:
             esp32_cs = DigitalInOut(board.D9)
             esp32_ready = DigitalInOut(board.D10)
@@ -82,20 +107,21 @@ class SensorLogger:
         status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
         self.wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(self._esp, self._secrets, status_light)
 
-    @_log_exceptions
+    # @_log_exceptions
     def _initialize_io(self):
-        aio_username = self._secrets["aio_username"]
-        aio_key = self._secrets["aio_key"]
+        # aio_username = self._secrets["aio_username"]
+        # aio_key = self._secrets["aio_key"]
+        aio_username = "siddacious"
+        aio_key = "6889f7e2f8fb4d7999cf7a844492d667"
 
         # Create an instance of the Adafruit IO HTTP client
         self.io = IO_HTTP(aio_username, aio_key, self.wifi)
-        self._initialize_feeds()
-
+        self._initialize_feeds(): 
     @_log_exceptions
     def _initialize_feeds(self):
         self.temperature_feed = self.io.get_feed("temperature")
         self.humidity_feed = self.io.get_feed("humidity")
-        self.batt_volts_feed = self.io.get_feed("batt-volts")
+        # self.batt_volts_feed = self.io.get_feed("batt-volts")
 
 
 
@@ -104,40 +130,43 @@ class SensorLogger:
         if not self.io:
             self._initialize_io
         # Create library object using our Bus I2C port
-        self.bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(self._i2c)
-        self.bmp280.mode = adafruit_bmp280.MODE_NORMAL
-        self.bmp280.overscan_temperature = adafruit_bmp280.OVERSCAN_X2
+        self.temp_sensor = PHT_SENSOR(self._i2c)
+        self.humidity_sensor = self.temp_sensor
+        # self.temp_sensor.mode = adafruit_bmp280.MODE_NORMAL
+        # self.temp_sensor.overscan_temperature = adafruit_bmp280.OVERSCAN_X2
 
-        time.sleep(1)
-        self.sht = adafruit_sht31d.SHT31D(self._i2c)
+
 
     @_log_exceptions
     def log_sensors(self):
         print("verifying feeds")
-        if not (self.humidity_feed and self.temperature_feed and self.batt_volts_feed):
+        # if not (self.humidity_feed and self.temperature_feed and self.batt_volts_feed):
+        if not (self.humidity_feed and self.temperature_feed):
             self._initialize_feeds()
-        temp = self.bmp280.temperature
-        hum = self.sht.relative_humidity
-        bv = volts(self.battery)
-        print("\033[1mBMP Tmemperature:\033[0m %0.3f k ºC" % self.sht.temperature)
+        temp = self.temp_sensor.temperature
+        # hum = self.humidity_sensor.relative_humidity
+        hum = self.humidity_sensor.humidity
+        # bv = volts(self.battery)
+        print("\033[1mBMP Tmemperature:\033[0m %0.3f k ºC" % self.humidity_sensor.temperature)
         print("\033[1mSHT Humidity:\033[0m %0.2f %%" % hum)
-        print("\033[1mBattery Voltage:\033[0m %0.2f V" % bv)
+        # print("\033[1mBattery Voltage:\033[0m %0.2f V" % bv)
         # Retrieve data value from the feed
         self.io.send_data(self.temperature_feed["key"], temp)
         self.io.send_data(self.humidity_feed["key"], hum)
-        self.io.send_data(self.batt_volts_feed["key"], bv)
+        # self.io.send_data(self.batt_volts_feed["key"], bv)
         print("Data sent!")
 
+print("shpongle")
+if __name__ == "__main__":
+    print("yes, __main__")
+    last_write = time.monotonic()
+    logger = SensorLogger(i2c_bus=i2c)
+    while True:
 
+        if abs(time.monotonic()-last_write >15):
+            last_write = time.monotonic()
+            logger.log_sensors()
+            print("took %d seconds to send sensor data"%(time.monotonic()-last_write))
 
-last_write = time.monotonic()
-logger = SensorLogger()
-while True:
-
-    if abs(time.monotonic()-last_write >15):
-        last_write = time.monotonic()
-        logger.log_sensors()
-        print("took %d seconds to send sensor data"%(time.monotonic()-last_write))
-
-# https://io.adafruit.com/api/v2/siddacious/feeds/batt-volts
-# https://io.adafruit.com/api/v2/siddacious/feeds/batt_voltsk
+    # https://io.adafruit.com/api/v2/siddacious/feeds/batt-volts
+    # https://io.adafruit.com/api/v2/siddacious/feeds/batt_voltsk
