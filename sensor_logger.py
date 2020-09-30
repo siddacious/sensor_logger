bc54dfe38acf04ccbf348dd8757d8c25070ec05a
siddacious <nospam187+github@gmail.com>
2020-09-30 01:24:19 -0700

    mooooooooar

--- sensor_logger.py
+++ sensor_logger.py
@@ -1,9 +1,23 @@
 """
-Sending data to Adafruit IO and receiving it.
+A more robust sensor logger
 """
+# TODOS:
+# show RSSI on screen
+# dim status neopixel
+# log errors to txt file w/ timestamp
+# show current tmp/hum in big nums
+# auto dim after delay
+# show additional status info via led: No power/batt only
+
+# verifying feeds
+# BMP Tmemperature: 24.900 k ºC
+# SHT Humidity: 55.00 %
+# Data sent!
+# took 8 seconds to send sensor data RSSI: -49
 from random import randint
 import time
 from collections import namedtuple
+import storage
 
 import analogio
 import board
@@ -13,10 +27,14 @@ from digitalio import DigitalInOut
 from adafruit_esp32spi import adafruit_esp32spi, adafruit_esp32spi_wifimanager
 from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
 import neopixel
-
-from adafruit_bme680 import Adafruit_BME680_I2C as PHT_SENSOR
-
 from gamepadshift import GamePadShift
+
+# from adafruit_shtc3 import SHTC3 as PHT_SENSOR
+# from adafruit_bme680 import Adafruit_BME680_I2C as PHT_SENSOR
+from adafruit_ahtx0 import AHTx0 as PHT_SENSOR
+
+
+
 
 import rtc
 
@@ -28,27 +46,11 @@ except AttributeError as attr:
 
 i2c = board.I2C()
 
-import rtc
-
-
 def _curr_time_str():
     current_time = time.localtime()
     current_time.tm_hour
     current_time.tm_min
     current_time.tm_sec
-
-
-# TODOS:
-# get time from IO
-# log errors to txt file
-# show current tmp/hum in big nums
-# auto dim after delay
-# show RSSI on screen
-# unblock requests
-# send
-# block on _wait_for_ready to verify ready pin is high
-# def _wait_response_cmd(self, cmd, num_responses=None, *, param_len_16=False):
-# def _wait_response_cmd(self, cmd, num_responses=None, *, param_len_16=False):
 
 ####################
 def volts(analog_in):
@@ -85,6 +87,7 @@ class SensorLogger:
         self.temperature_feed = None
         self.humidity_feed = None
         self.batt_volts_feed = None
+        self.heater_state = False
 
     def _log_exceptions(func):
         # pylint:disable=protected-access
@@ -96,12 +99,11 @@ class SensorLogger:
             except Exception as e:
                 err_str = "ERROR in %s\n%s\n" % (func.__name__, e)
                 if self._settings["log_errors_to_file"]:
-                    import storage
 
-                    storage.remount("/", False)
+                    storage.remount("/", disable_concurrent_write_protection=False)
                     with open(self._error_log_file, "a") as err_log:
                         err_log.write(err_str)
-                    storage.remount("/", True)
+                    storage.remount("/", False)
                 print(err_str)
 
             return retval
@@ -125,20 +127,21 @@ class SensorLogger:
             esp32_ready = DigitalInOut(board.D11)
             esp32_reset = DigitalInOut(board.D12)
         except AttributeError:
-            esp32_cs = DigitalInOut(board.D9)
-            esp32_ready = DigitalInOut(board.D10)
-            esp32_reset = DigitalInOut(board.D5)
+            esp32_cs = DigitalInOut(board.ESP_CS)
+            esp32_ready = DigitalInOut(board.ESP_BUSY)
+            esp32_reset = DigitalInOut(board.ESP_RESET)
 
         self._spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
         self._esp = adafruit_esp32spi.ESP_SPIcontrol(
             self._spi, esp32_cs, esp32_ready, esp32_reset, service_function=self.service
         )
-        status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+        status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
         self.wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(
             self._esp, self._secrets, status_light
         )
 
     def service(self):
+        # add touch_screen_service
         button_machine.service()
         screen_machine.service()
 
@@ -155,8 +158,11 @@ class SensorLogger:
 
     @_log_exceptions
     def _initialize_feeds(self):
-        self.temperature_feed = self.io.get_feed("temperature")
-        self.humidity_feed = self.io.get_feed("humidity")
+        self.temperature_feed = self.io.get_feed(self._secrets["aio_temp_feed"])
+        self.humidity_feed = self.io.get_feed(self._secrets["aio_hum_feed"])
+        self.rssi_feed = self.io.get_feed(self._secrets["aio_rssi_feed"])
+        if 'aio_heater_feed' in self._secrets:
+            self.heater_feed = self.io.get_feed(self._secrets["aio_heater_feed"])
         # self.batt_volts_feed = self.io.get_feed("batt-volts")
 
     @_log_exceptions
@@ -168,22 +174,30 @@ class SensorLogger:
 
     @_log_exceptions
     def log_sensors(self):
-        print("verifying feeds")
         if not (self.humidity_feed and self.temperature_feed):
             self._initialize_feeds()
         temp = self.temp_sensor.temperature
-        hum = self.humidity_sensor.humidity
-
+        hum = self.humidity_sensor.relative_humidity
+        rssi = self.wifi.signal_strength()
         print(
-            "\033[1mBMP Tmemperature:\033[0m %0.3f k ºC"
-            % self.humidity_sensor.temperature
+            "\033[1mTmemperature:\033[0m %0.3f k ºC"
+            % temp
         )
-        print("\033[1mSHT Humidity:\033[0m %0.2f %%" % hum)
+        print("\033[1mHumidity:\033[0m %0.2f %%" % hum)
+        print("\033[1mRSSI:\033[0m %0.2f" % rssi)
+        if self.heater_feed:
+            if temp < 29.0:
+                self.heater_state = True
+            elif temp > 31.0:
+                self.heater_state = False
+
+            print("\033[1mHeater on:\033[0m", self.heater_state)
+            self.io.send_data(self.heater_feed["key"], self.heater_state)
 
         self.io.send_data(self.temperature_feed["key"], temp)
         self.io.send_data(self.humidity_feed["key"], hum)
-
-        print("Data sent!")
+        self.io.send_data(self.rssi_feed["key"], rssi)
+       
 
 
 if __name__ == "__main__":
@@ -206,10 +220,11 @@ if __name__ == "__main__":
     logger = SensorLogger(i2c_bus=i2c)
     while True:
 
-        logger.service()
         if abs(time.monotonic() - last_write > 15):
             last_write = time.monotonic()
             logger.log_sensors()
+            logger.service()
             print(
-                "took %d seconds to send sensor data" % (time.monotonic() - last_write)
+                "took %d seconds to send sensor data" % (time.monotonic() - last_write),
             )
+            print("")
